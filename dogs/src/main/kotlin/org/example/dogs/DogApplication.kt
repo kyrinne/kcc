4f02d54177ea1e2b85590f6ecadb99c34a1def51
cyrinne <174140235+kyrinne@users.noreply.github.com>
2025-11-17 09:59:16 +0100

    Use enqueue / callbacks to show the message before displaying the image.

--- dogs/src/main/kotlin/org/example/dogs/DogApplication.kt
+++ dogs/src/main/kotlin/org/example/dogs/DogApplication.kt
@@ -1,13 +1,25 @@
 package org.example.dogs
 
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.swing.JFrame
+import javax.swing.SwingUtilities
+
+fun interface ImageViewerCallback<T> : Callback<T> {
+    override fun onResponse(call: Call<T>, response: Response<T>)
+    override fun onFailure(call: Call<T>, t: Throwable) = t.printStackTrace()
+}
 
 fun loadDogImage(window: JFrame) {
-    val infoRequest = dogService.getPictures(2)
-    val infoResponse = infoRequest.execute()
-    val imageUrl = infoResponse.body()!!.message.first() // TODO: handle number
-    val imageRequest = dogService.downloadImage(imageUrl)
-    val imageResponse = imageRequest.execute()
-
-    window.displayImage(imageResponse.body()!!.bytes())
+    val infoRequest = dogService.getPictures(1)
+    infoRequest.enqueue(ImageViewerCallback { _, infoResponse ->
+        val imageUrl = infoResponse.body()!!.message.first() // TODO: handle number
+        val imageRequest = dogService.downloadImage(imageUrl)
+        imageRequest.enqueue(ImageViewerCallback { _, imageResponse ->
+            SwingUtilities.invokeLater {
+                window.displayImage(imageResponse.body()!!.bytes())
+            }
+        })
+    })
 }
